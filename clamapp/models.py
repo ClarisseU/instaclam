c0f74ae871676b5ff3008169f1c20a4e2b14c482
ClarisseU <clarisseum@gmail.com>
2019-10-17 12:05:35 +0200

    add a search method in image

--- clamapp/models.py
+++ clamapp/models.py
@@ -14,6 +14,8 @@ class Profile(models.Model):
     
     def delete_profile(self):
         self.delete()
+        
+       
     
 class Comments(models.Model):
     comment_cont = models.CharField(max_length=120)
@@ -47,6 +49,9 @@ class Image(models.Model):
     def update_caption(self):
         self.update()
     
+    @classmethod
+    def search(cls,id):
+        pass 
         
             
 
