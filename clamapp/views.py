073bf20142cb5dc2917329f1eaf194ab7ef8110f
ClarisseU <clarisseum@gmail.com>
2019-10-18 21:11:31 +0200

    add some views

--- clamapp/views.py
+++ clamapp/views.py
@@ -3,7 +3,7 @@ import datetime as datetime
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Image,Profile,Comments
 from django.contrib.auth.decorators import login_required
-from .forms import NewPostForm
+from .forms import NewPostForm,ProfileForm
 
 @login_required(login_url='/accounts/login/')
 def welcome(request):
@@ -32,4 +32,47 @@ def new_post(request):
     
     else:
         form = NewPostForm()
-    return render (request, 'new_post.html', {"form":form})        
+    return render (request, 'new_post.html', {"form":form})
+
+@login_required(login_url='/accounts/login')
+def profile(request):
+    current_user = request.user
+    image = Image.objects.filter(user=current_user)
+    profile = Profile.objects.filter(user=current_user).first()
+    return render(request,'nu_profile.html',{"image":image,"profile":profile})
+
+@login_required(login_url='/accounts/login')    
+def nu_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        return redirect('profile')  
+    else:
+        form = ProfileForm()
+    return render(request, 'profile.html', {"form":form}) 
+
+@login_required(login_url='/accounts/login')
+def profile(request):
+    current_user = request.user
+    image = Image.objects.filter(user=current_user).all()
+    profile = Profile.objects.filter(user=current_user).first()
+    return render(request,'nu_profile.html',{"image":image,"profile":profile})
+ 
+@login_required(login_url='/accounts/login/')
+def search(request):
+   if 'username' in request.GET and request.GET["username"]:
+       search_term = request.GET.get("username")
+       searched_users = Profile.search_by_profile(search_term)
+       print(searched_users)
+       message = f"{search_term}"
+       return render(request, "search.html",{"message":message,"users": searched_users})
+   else:
+       message = "You haven't searched for any term"
+       return render(request, 'search.html',{"message":message})
+            
+            
+                
