fc0e97a548a89f047afb35654378aff7b4d42c96
ClarisseU <clarisseum@gmail.com>
2019-10-20 19:12:57 +0200

    add urlConf for comment

--- clamapp/urls.py
+++ clamapp/urls.py
@@ -13,7 +13,8 @@ urlpatterns = [
     url(r'^new/post$',views.new_post, name ='new-post'),
     url(r'^new/profile', views.nu_profile,name='profile'),
     url(r'^new/nuprofile', views.profile,name='profiles'),
-    url(r'^search',views.search, name='search')
+    url(r'^search',views.search, name='search'),
+    url(r'^new/comment/(\d+)/$',views.comment, name ='comment')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
