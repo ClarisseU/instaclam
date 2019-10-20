130c8e3f363cc1024351b7fb47e91227df1619ab
ClarisseU <clarisseum@gmail.com>
2019-10-20 19:13:37 +0200

    add commentform

--- clamapp/forms.py
+++ clamapp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Image,Profile
+from .models import Image,Profile, Comments
 
 class NewPostForm(forms.ModelForm):
     class Meta:
@@ -13,3 +13,11 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['user_id']
+        
+class commentForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        exclude = ['commented_by','commented_image']
+        widgets = {
+            'likes': forms.CheckboxSelectMultiple(),
+        }        
