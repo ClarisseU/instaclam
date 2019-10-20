b8747881f0f586d2b36ff2671e88ec0332fe47e4
ClarisseU <clarisseum@gmail.com>
2019-10-20 19:12:04 +0200

    add comment method

--- clamapp/views.py
+++ clamapp/views.py
@@ -3,13 +3,17 @@ import datetime as datetime
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Image,Profile,Comments
 from django.contrib.auth.decorators import login_required
-from .forms import NewPostForm,ProfileForm
+from .forms import NewPostForm,ProfileForm, commentForm
 
 @login_required(login_url='/accounts/login/')
 def welcome(request):
     images = Image.objects.all()
     print(images)
-    return render(request, 'welcome.html', {'images':images})
+    user_profile = Profile.objects.all()
+    current_user = request.user
+    profile = Profile.objects.filter(id=current_user.id).first()
+    comment = Comments.objects.filter(id = current_user.id).first()
+    return render(request, 'welcome.html', {'images':images, 'user_profile':user_profile, 'profile': profile, 'comment':comment})
 
 @login_required(login_url='/accounts/login')
 def post(request,id):
@@ -34,12 +38,12 @@ def new_post(request):
         form = NewPostForm()
     return render (request, 'new_post.html', {"form":form})
 
-@login_required(login_url='/accounts/login')
-def profile(request):
-    current_user = request.user
-    image = Image.objects.filter(user=current_user)
-    profile = Profile.objects.filter(user=current_user).first()
-    return render(request,'nu_profile.html',{"image":image,"profile":profile})
+# @login_required(login_url='/accounts/login')
+# def profile(request):
+#     current_user = request.user
+#     image = Image.objects.filter(user=current_user)
+#     profile = Profile.objects.filter(user=current_user).first()
+#     return render(request,'nu_profile.html',{"image":image,"profile":profile})
 
 @login_required(login_url='/accounts/login')    
 def nu_profile(request):
@@ -64,9 +68,9 @@ def profile(request):
  
 @login_required(login_url='/accounts/login/')
 def search(request):
-   if 'username' in request.GET and request.GET["username"]:
-       search_term = request.GET.get("username")
-       searched_users = Profile.search_by_profile(search_term)
+   if 'user' in request.GET and request.GET["user"]:
+       search_term = request.GET.get("user")
+       searched_users = Profile.search(search_term)
        print(searched_users)
        message = f"{search_term}"
        return render(request, "search.html",{"message":message,"users": searched_users})
@@ -74,5 +78,22 @@ def search(request):
        message = "You haven't searched for any term"
        return render(request, 'search.html',{"message":message})
             
-            
+@login_required(login_url='/accounts/login')
+def comment(request, img_id):
+    current_user = request.user
+    image = Image.objects.filter(id = img_id).first()
+    print(image)
+    prfl = Profile.objects.filter(user = current_user.id).first()
+    print(prfl)
+    if request.method == 'POST':
+        form = commentForm(request.POST, request.FILES)
+        if form.is_valid():
+            comment = form.save(commit = False)
+            comment.commented_by = prfl
+            comment.commented_image = image
+            comment.save()
+            return redirect('welcome')
+    else:
+        form = commentForm()
+    return render(request, 'commentform.html', {'form': form, 'img_id':img_id})    
                 
